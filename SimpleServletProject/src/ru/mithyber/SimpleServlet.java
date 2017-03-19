a054d7168c516877537c9448a764eb8110c8b946
MiTHyber <shevva1304@yahoo.com>
2017-03-19 17:34:06 +0300

    upd

---
+++ SimpleServletProject/src/ru/mithyber/SimpleServlet.java
@@ -0,0 +1,42 @@
+package ru.mithyber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		HttpSession session = request.getSession();
+		String userName = request.getParameter("userName");
+		ServletContext context = request.getServletContext();
+		if (!"".equals(userName) && userName != null) {
+			session.setAttribute("userName", userName);
+			context.setAttribute("userName", userName);
+		}
+		writer.println("Request parameter has userName as " + userName);
+		writer.println("Session parameter has userName as " + session.getAttribute("userName"));
+		writer.println("Context parameter has userName as " + context.getAttribute("userName"));
+	}
+
+}
---
+++ SimpleServletProject/src/ru/mithyber/XmlServlet.java
@@ -0,0 +1,34 @@
+package ru.mithyber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		String userName = req.getParameter("userName");
+		writer.println("Hello from Get, " + userName + "!");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		String userName = req.getParameter("userName");
+		String fullName = req.getParameter("fullName");
+		writer.println("Hello from POST, " + userName + "! We know your full name " + fullName);
+		String prof = req.getParameter("prof");
+		writer.println("You are a(n) " + prof);
+		String[] locations = req.getParameterValues("location");
+		writer.println("You are " + Arrays.toString(locations));
+	}
+}
